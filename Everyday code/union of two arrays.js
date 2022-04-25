699a56a16cc5c3b643a4a4f504552ed80683b732
Shreepad Avhad <Shripad.avhad@gmail.com>
2022-04-25 13:20:55 +0530

    union of array

---
+++ Everyday code/union of two arrays.js
@@ -0,0 +1,31 @@
+// javascript program for the union of two arrays using Set
+function getUnion(a, n, b, m) {
+    // find min of n and m
+    var min = (n < m) ? n : m;
+    var set = new Set();
+    for (i = 0; i < min; i++) {
+        set.add(a[i]);
+        set.add(b[i]);
+    }
+
+    if (n > m) {
+        for (i = m; i < n; i++) {
+            set.add(a[i]);
+        }
+    } else if (n < m) {
+        for (i = n; i < m; i++) {
+            set.add(b[i]);
+        }
+    }
+
+    document.write("The union of both arrays is :<br />");
+    set.forEach(function (value) {
+        document.write(value + " ");
+    })
+
+}
+
+var a = [1, 2, 5, 6, 2, 3, 5, 7, 3];
+var b = [2, 4, 5, 6, 8, 9, 4, 6, 5, 4];
+
+getUnion(a, 9, b, 10);
